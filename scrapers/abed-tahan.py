78d4e7e11059d5c202b236d33dd91bb1ac6149bf
alexchbib <133498896+alexchbib@users.noreply.github.com>
2025-01-05 18:32:36 +0200

    Create abed-tahan.py

---
+++ scrapers/abed-tahan.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+search= input("enter_item: ")
+search=search.split()
+searchz = "+".join(search)
+url = "https://abedtahan.com/search?q=" + searchz +"&options%5Bprefix%5D=last"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "lxml")
+everything = soup.find("div", class_ = "product-grid-container")
+urls=[]
+
+urls = everything.find_all("a", class_ ="full-unstyled-link")
+
+links=[]
+for z in urls:
+    relative_link = z.get("href")
+    link = "https://abedtahan.com" + relative_link
+    links.append(link)
+
+
+
+L=[]
+for item_link in links:
+    currDic= {}
+    page2 = requests.get(item_link)
+    soup2 = BeautifulSoup(page2.text, "lxml") 
+    everything2 = soup2.find("section", class_ = "shopify-section section")
+    prices = everything2.find("div", class_="price__sale")
+    if prices != None:
+        currDic["Name"] = everything2.find("h1").text.strip()
+        currDic["Final price"]= prices.find("span", class_ = "price-item price-item--sale price-item--last").text.strip()
+        currDic["Original Price"] = prices.find("small", class_ ="price-item price-item--regular").text.strip()
+        L.append(currDic)
+        print(currDic)
+
+
